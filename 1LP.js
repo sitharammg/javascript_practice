b494db5f39e84a7308b4fe20dc777e312a2c47be
udaykumar <uday.sivapuramvenkata@mobigesture.com>
2019-02-25 10:30:38 +0530

    Changes to 1LP Program adding Errors handling to it.

--- 1LP.js
+++ 1LP.js
@@ -3,8 +3,11 @@ var fct = 1;
 
 function factorial() {
   if (num.value.trim() == "") {
-    document.getElementById("result").innerHTML = "Number not Provided";
-    console.log("Number not Provided")
+    
+    document.getElementById("result").innerHTML =  new Error("Number not Provided!");
+   throw new Error("Number not Provided!");
+   //document.getElementById("result").innerHTML =  "Number not Provided!";
+    //console.log("Number not Provided")
   }
   else if (num.value > 0 && num.value < 100) {
     for (var i = 1; i <= num.value; ++i)
@@ -14,7 +17,9 @@ function factorial() {
     console.log(" Factorial of " + num.value + " is " + fct);
   }
   else {
-    document.getElementById("result").innerHTML = "Number provided is not in range 0 - 100";
-    console.log("Number provided is not in range 0 - 100")
+    document.getElementById("result").innerHTML =  new Error("Number Provided is not in range 0 - 100!");
+    throw new Error("Number Provided is not in range 0 - 100!");
+    // document.getElementById("result").innerHTML = "Number provided is not in range 0 - 100";
+    // console.log("Number provided is not in range 0 - 100")
   }
 }
